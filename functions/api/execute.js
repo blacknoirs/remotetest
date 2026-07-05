0f89525e85f5b3f647ac6e12f8768f1ab534550e
NetlessMade <renderv2lol@gmail.com>
2026-07-05 02:38:29 +0000

    test

--- functions/api/execute.js
+++ functions/api/execute.js
@@ -1,15 +1,18 @@
-let script = "";
+let scripts = {};
 
 export async function onRequestPost({request}) {
     try {
         let data = await request.json();
-        script = data.script || "";
+        let key = data.key;
+        if (key) scripts[key] = data.script || "";
         return new Response(JSON.stringify({ok:true}));
     } catch(e) {
         return new Response(null, {status:400});
     }
 }
 
-export async function onRequestGet() {
-    return new Response(JSON.stringify({script: script}));
+export async function onRequestGet({request}) {
+    let url = new URL(request.url);
+    let key = url.searchParams.get("key") || "";
+    return new Response(JSON.stringify({script: scripts[key] || ""}));
 }
