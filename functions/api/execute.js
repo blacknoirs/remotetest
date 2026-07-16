6133bb85cabafb693e88de87416e516cfb09b5fd
NetlessMade <renderv2lol@gmail.com>
2026-07-16 17:44:23 +0000

    test

--- functions/api/execute.js
+++ functions/api/execute.js
@@ -1,17 +1,10 @@
 let scripts = {};
-let statsData = {};
 
 export async function onRequestPost({request}) {
     try {
         let data = await request.json();
         let key = data.key;
-        if (key) {
-            if (data.kills !== undefined) {
-                statsData[key] = {kills: data.kills, total: data.total || 0};
-            } else {
-                scripts[key] = data.script || "";
-            }
-        }
+        if (key) scripts[key] = data.script || "";
         return new Response(JSON.stringify({ok:true}));
     } catch(e) {
         return new Response(null, {status:400});
@@ -23,12 +16,9 @@ export async function onRequestGet({request}) {
     let key = url.searchParams.get("key") || "";
     let clear = url.searchParams.get("clear");
     
-    if (url.pathname.includes("/stats")) {
-        return new Response(JSON.stringify(statsData[key] || {kills:0,total:0}));
-    }
-    
     let current = scripts[key] || "";
-    if (clear === "1") scripts[key] = "";
-    
+    if (clear === "1") {
+        scripts[key] = "";
+    }
     return new Response(JSON.stringify({script: current}));
 }
